8477255eaf5b0f7fc9c984346dd5090b28655920
loitd <loitranduc@gmail.com>
2019-06-19 09:25:48 +0700

    lauto.sh

--- lutils/lutils.py
+++ lutils/lutils.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
+# -------------------------------------------------------
+# lutils library
+# Author: Tran Duc Loi (loitranduc@gmail.com)
+# Git: https://github.com/loitd/lutils
+# Documented follow PEP257 
+# -------------------------------------------------------
 from datetime import date, timedelta, datetime
 
-def todaystr(format="%Y/%m/%d %H:%M:%S"):
+
+def datetimestr(format="%Y/%m/%d %H:%M:%S"):
+    """Return current datetime in specific format."""
     return(datetime.now().strftime(format))
 
 #yesterday
 def yesterday():
+    """Print yesterday date in datetime value"""
     return (date.today() - timedelta(days=1))
 
 def yesterdaystr(format="%Y/%m/%d"):
-    # print yesterday datetime
+    """Print yesterday datetime in string format."""
     return(yesterday().strftime(format))
 
-def printx(content, filepath):
+def printx(content, filepath="./log.txt"):
+    """Print to screen output AND write to log file. From version 1.0, by default log file path is: ./log.txt
+    Will be moved to printlog(). Please use printlog() instead of printx() with the same syntax."""
+    try:
+        print("{0}".format(content))
+        with open(filepath, "a+") as fh:
+            fh.write("[{0}]: {1}\r\n".format(todaystr(), content))
+    except Exception as e:
+        raise(e)
+
+def printlog(content, filepath="./log.txt"):
+    """Print to screen output AND write to log file. Added from version 1.0.
+    By default log file path is: ./log.txt"""
     try:
         print("{0}".format(content))
         with open(filepath, "a+") as fh:
@@ -21,4 +42,4 @@ def printx(content, filepath):
         raise(e)
 
 if __name__ == "__main__":
-    print(todaystr())
+    print(datetimestr())
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='lutils',  
-     version='0.9',
+     version='1.0',
      #this will add to \Scripts folder
      #scripts=['lutils.py'] ,
      author="Tran Duc Loi",
